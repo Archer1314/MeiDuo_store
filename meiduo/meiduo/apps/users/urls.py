112966cd3dae73bcc7827529049e4d2c73c927ee
pgh <pangguohao@qq.com>
2019-07-04 21:18:32 +0800

    注册校验

--- meiduo/meiduo/apps/users/urls.py
+++ meiduo/meiduo/apps/users/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
 
     url(r'^register/$', views.Users.as_view()),
+    url(r'^usernames/[a-zA-Z0-9_-]{5,20}/count/$', views.Users.as_view()),
 ]
--- meiduo/meiduo/apps/users/views.py
+++ meiduo/meiduo/apps/users/views.py
@@ -1,7 +1,10 @@
+from django.contrib.auth import login
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django import http
 # Create your views here.
+import re
+from .models import User
 
 from django.views.generic.base import View
 
@@ -9,3 +12,48 @@ from django.views.generic.base import View
 class Users(View):
     def get(self, request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        query_dict = request.POST
+        print(query_dict)
+        username = query_dict.get('username', '')
+        password = query_dict.get('password', '')
+        password2 = query_dict.get('password2', '')
+        telephone = query_dict.get('mobile', '')
+        #  图形验证码只是用来防止恶意大量注册使用,
+        #  逻辑是填完校验码后点击获取手机验证码时将填的校验码发到后端进行比较,
+        #  确认一致才发送短信(短信收费),所以不需要作为个人信息进行校验和保险到数据库
+        # image_code = query_dict.get('image_code', '')
+        sms_code = query_dict.get('sms_code', '')
+        allow = query_dict.get('allow', '')
+
+        # 校验有无选项为空,checkbox标签在提交时默认提供None/on, 或用value属性指定提交的信息
+        # 此处register.html使用默认,以下all可判断是否勾选,无需进行其他校验.
+        if not all(query_dict.dict().values()):
+            return http.HttpResponseForbidden('非法注册,部分为空')
+        # if not all([username, password, password2, telephone, sms_code, allow]):
+        #     return http.HttpResponseForbidden('非法注册,部分为空')
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return http.HttpResponseForbidden('非法注册,用户名不符合规范')
+        if not re.match(r'^[[0-9A-Za-z]{8,20}$', password):
+            return http.HttpResponseForbidden('非法注册,密码不符合规范')
+        if not password == password2:
+            return http.HttpResponseForbidden('非法注册,密码两次输入不一致')
+        if not re.match(r'^1[345789]\d{9}$', telephone):
+            return http.HttpResponseForbidden('非法注册,手机号不符合规范')
+        # TODO:if sms_code短信校验码验证还未写.
+
+        user = User.objects.create_user(username=username, password=password, telephone= telephone)
+        login(request, user)
+
+        return redirect('http://www.baidu.com')
+
+
+class UsernamecodeView(View):
+    def get(self, request, username):
+        username = username
+        count = User.objects.filter(username=username)
+        return http.JsonResponse({'count': count})
+
+
+
